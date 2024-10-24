4a3c5eabd795a2e34e623c4bddf8b1cbaad660ca
Ananya Shahrin Promi <ananyapromi3@gmail.com>
2024-10-24 10:23:43 +0600

    first commit 2

---
+++ services/backend/src/routes/products.js
@@ -0,0 +1,10 @@
+// src/routes/products.js
+
+import express from "express";
+import productsController from "../controllers/Products.Controller.js"
+const router = express.Router();
+
+router.get('/',  productsController.index);
+router.post('/',  productsController.save);
+
+export default router;
